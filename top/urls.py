1e42505330c1784aa5ec4fb041409411c75bebd2
MuratVh <murat-1434@mail.ru>
2023-09-13 22:00:02 +0300

    murat

--- top/urls.py
+++ top/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -12,3 +11,7 @@ urlpatterns = [
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
+
+
+
+
